fc94d04ed5c2be82b0bf162c0c2fad45ce949803
hsnpuply <hsnpuply@gmail.com>
2023-11-19 22:48:52 +0330

    Tasks
    Task Complete and Uncomplete and showing the time of task done

--- script.js
+++ script.js
@@ -1,24 +1,35 @@
 const inputBox = document.getElementById('input-box');
 const listContainer = document.getElementById('list-container');
+let date = new Date();
+
+
+
+let time= date.toLocaleString('en-US', {
+    hour12: true,
+    hour: '2-digit',
+    minute: '2-digit',
+});
 
 
 function addTask(){
     if(inputBox.value===""){
+        inputBox.focus();
         alert("Please enter a task");
     }else{
         let li = document.createElement("li");
-        li.innerHTML= inputBox.value;
+        let p = document.createElement("p")
+        p.setAttribute("class","p-taskDescription")
+        p.innerHTML= inputBox.value;
+        li.appendChild(p);
         listContainer.appendChild(li);
-        let span = document.createElement("span")
-        span.innerHTML = "\u00d7"
-        li.appendChild(span)
+        let close_btn = document.createElement("span")
+        close_btn.innerHTML = "\u00d7"
+        li.appendChild(close_btn)
         saveData();
     }
     inputBox.value=''
     saveData();
     inputBox.focus();
-
-
 }
 
 listContainer.addEventListener('dblclick',(e)=>{
@@ -27,20 +38,33 @@ listContainer.addEventListener('dblclick',(e)=>{
     inputBox.focus();
         saveData();
     }
-})
+});
+
 listContainer.addEventListener("click",(e)=>{
+    inputBox.focus();
+
     if(e.target.tagName==="LI"){
-        e.target.classList.toggle("checked")
-        inputBox.focus();
-        saveData();
-    }else if(e.target.tagName === "SPAN"){
+    e.target.classList.toggle("checked")
+    if(e.target.classList.contains('checked')){
+        let task_done_time = document.createElement("h4");
+        task_done_time.setAttribute("class","task_done");
+        task_done_time.innerHTML = "Task Done in : "+ time;
+        e.target.appendChild(task_done_time);
+    }else{
+        let task_done_time = document.querySelector(".task_done");
+        task_done_time.parentNode.removeChild(task_done_time);
+    }
+    inputBox.focus();
+    saveData();
+    }
+     if(e.target.tagName === "SPAN"){
         e.target.parentElement.remove();
         inputBox.focus();
         saveData();
     }
-},false)
+},false);
 
-
+// clearData()
 
 function saveData(){
     localStorage.setItem("data",listContainer.innerHTML)
@@ -48,7 +72,13 @@ function saveData(){
 
 function showTask(){
     listContainer.innerHTML = localStorage.getItem("data")
+
 }
+
+function clearData(){
+    localStorage.setItem("data",listContainer.innerHTML="")
+}
+
 
 
 inputBox.addEventListener("keyup",(e)=>{
@@ -58,3 +88,4 @@ inputBox.addEventListener("keyup",(e)=>{
 })
 
 showTask();
+
